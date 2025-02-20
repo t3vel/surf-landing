34688d67c5204375e53f0e18a5c0191773c12f04
Maks <maksim.oleynik2017@gmail.com>
2025-02-20 17:19:00 +0200

    add animations

--- js/main.js
+++ js/main.js
@@ -4,3 +4,20 @@ const menu = document.querySelector('.menu');
 menuBtn.addEventListener('click', () => {
   menu.classList.toggle('menu--open');
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const sections = document.querySelectorAll('.fade-in');
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+        }
+      });
+    },
+    { threshold: 0.3 }
+  );
+
+  sections.forEach((section) => observer.observe(section));
+});
